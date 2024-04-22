5e0fdff60229b65c03eab1bea358cb5cf575ad76
Cate Haley <s105801@ltisdschools.net>
2024-04-22 11:50:53 -0500

    Added coins

--- Assets/Scripts/EnemySpawnerScript.cs
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -5,31 +5,52 @@ using UnityEngine;
 public class EnemySpawnerScript : MonoBehaviour
 {
     public GameObject enemy;
-    [SerializeField] private GameObject EnemySpawner;
+    [SerializeField] private GameObject Spawner;
     public float speed;
-    public float spawnRate = 2;
-    private float timer = 0;
-    private int count = 1;
+    public float spawnRate1 = 2;
+    private float timer1 = 0;
+    private int count1 = 1;
+    private float timer2 = 0;
+    private int count2 = 1;
+    public float spawnRate2 = 2;
+    public GameObject coin;
 
 
     // Update is called once per frame
     void Update()
     {
-        EnemySpawner.transform.position = new Vector3(EnemySpawner.transform.position.x - speed, EnemySpawner.transform.position.y, EnemySpawner.transform.position.z);
+        Vector3 pos = new Vector3(Spawner.transform.position.x - speed, Spawner.transform.position.y, Spawner.transform.position.z);
+        Spawner.transform.position = pos;
 
-        if (timer < spawnRate)
+        if (timer1 < spawnRate1)
         {
-            timer += Time.deltaTime;
+            timer1 += Time.deltaTime;
         }
         else
         {
-            GameObject badGuy = Instantiate(enemy, transform.position, transform.rotation);
-            badGuy.name = "Enemy" + count;
-            count++;
-            timer = 0;
+            Vector3 posEnemy = pos;
+            posEnemy.z += Random.Range(-7.5f, 7.5f);
+            Instantiate(enemy, posEnemy, transform.rotation);
+            count1++;
+            timer1 = 0;
             
         }
-        
+
+        if (timer2 < spawnRate2)
+        {
+            timer2 += Time.deltaTime;
+        }
+        else
+        {
+            Vector3 posCoin = pos;
+            posCoin.z += Random.Range(-7.5f, 7.5f);
+            posCoin.y = 1.5f;
+            Instantiate(coin, posCoin, transform.rotation);
+            count2++;
+            timer2 = 0;
+
+        }
+
 
 
     }
--- Assets/Scripts/LooseHealth_Player.cs
+++ Assets/Scripts/LooseHealth_Player.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LooseHealth_Player : MonoBehaviour
 {
-    public int health = 1;
+    public int health = 3;
+    public Text healthText;
+
 
     // Update is called once per frame
     void Update()
     {
+        healthText.text = "Health: " + health.ToString("0");
         if (health == 0)
         {
             Destroy(gameObject);

74393a18d5e4f2d7e9e4f757649bff0462583cce
Cate Haley <s105801@ltisdschools.net>
2024-04-24 11:34:31 -0500

    Added start and end screens

--- Assets/Scripts/LooseHealth_Player.cs
+++ Assets/Scripts/LooseHealth_Player.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LooseHealth_Player : MonoBehaviour
 {
     public int health = 3;
     public Text healthText;
+    public GameObject gameOverUI;
 
 
     // Update is called once per frame
@@ -15,7 +17,7 @@ public class LooseHealth_Player : MonoBehaviour
         healthText.text = "Health: " + health.ToString("0");
         if (health == 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(2);
         }
     }
     void OnTriggerEnter(Collider collision)
